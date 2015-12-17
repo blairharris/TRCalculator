fa51fb0e057ce8ffe5ec5f82ebb558f1bda6a7b7
Blair Harris <jonathan.blair.harris@gmail.com>
2015-12-17 18:35:23 +0000

    New file for eval tests

---
+++ MathExpressionParser.Tests/PostFixEvaluatorTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace MathExpressionParser.Tests
+{
+    [TestClass]
+    public class PostFixEvaluatorTest
+    {
+        readonly IPostFixEvaluator _evaluator = new PostFixEvaluator();
+
+        [TestMethod]
+        public void TwoPlusTwo()
+        {
+            var result = _evaluator.CalculationResult("22+");
+            result.Should().Be(4);
+        }
+
+        [TestMethod]
+        public void TwoPlusTwoTimes5()
+        {
+            var result = _evaluator.CalculationResult("225*+");
+            result.Should().Be(12);
+        }
+    }
+}

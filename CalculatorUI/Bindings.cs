4b50f657311429994c20d5e35616a032fbfc78e8
Blair Harris <jonathan.blair.harris@gmail.com>
2015-12-19 11:31:38 +0000

    Added simple WPF user interface

---
+++ CalculatorUI/Bindings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+using Ninject.Modules;
+using MathExpressionParser;
+
+namespace CalculatorUI
+{
+    public class Bindings : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IMathExpressionEvaluator>().To<MathExpressionEvaluator>();
+            Bind<IPostfixConverter>().To<PostFixConverter>();
+            Bind<IPostFixEvaluator>().To<PostFixEvaluator>();
+        }
+    }
+}
---
+++ CalculatorUI/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MathExpressionParser;
+using Ninject;
+using System.Reflection;
+
+namespace CalculatorUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IMathExpressionEvaluator _evaluator;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            IKernel kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+
+            _evaluator = kernel.Get<IMathExpressionEvaluator>();
+        }
+
+        private void evalueuateButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int result = _evaluator.CalculationResult(inputTextBox.Text);
+                outputTextBox.Text = result.ToString();
+            }
+            catch(MathExpressionException mathEx)
+            {
+                outputTextBox.Text = mathEx.Message;
+            }
+            catch(Exception generalEx)
+            {
+                outputTextBox.Text = generalEx.Message;
+            }
+        }
+    }
+}
